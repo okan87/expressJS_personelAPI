b65f8fdb9bfc03c412456a6fe491a1599375376c
okan87 <okankaplan1907@gmail.com>
2025-06-08 21:58:30 +0200

    project structre is created

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/* --------------------------------
+! EXPRESSJS - BLOGAPI PROJECT
+-------------------------------- */
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const dbConnection = require('./src/configs/dbConnection');
+const errorHandler = require('./src/middlewares/errorHandler');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// JSON parser middleware
+app.use(express.json());
+
+// Searching&Sorting&Pagination:
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+// Root endpoint
+app.all('/', (req, res) => {
+    res.send('Personel API');
+});
+
+
+
+
+//
+// Centralized error handler (should be last middleware)
+app.use(errorHandler);
+
+// Synchronization:
+// require('./src/sync')()
+
+// Connect to DB and start server
+dbConnection()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running http://127.0.0.1:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to DB:", err.message);
+        process.exit(1);
+    });
+
